a325f6f7c2f8fe85763f001593e41ae7f69345e7
Chris Noden <chris@noden.net>
2020-05-22 21:34:47 +0100

    Updated rules

--- src/rules.php
+++ src/rules.php
@@ -1,11 +1,13 @@
 <?php
 
 // last reviewed: v2.16.3 Yellow Bird
+// full list of rules at: https://github.com/FriendsOfPhp/PHP-CS-Fixer
 
 return [
     '@PSR2' => true,
     'array_syntax' => ['syntax' => 'short'],
+    'blank_line_before_statement' => true,
+    'concat_space' => ['spacing' => 'one'],
     'final_class' => false,
     'new_with_braces' => true,
-    // ...
 ];
